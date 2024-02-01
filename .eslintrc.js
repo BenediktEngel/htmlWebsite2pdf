2ba9cb0070862a6eb115d418bc1aef033e9ebe0e
benediktengel <info@benediktengel.de>
2024-02-01 11:16:51 +0100

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/indent': 'off',
+    'prefer-arrow-callback': 'warn',
+    'no-underscore-dangle': 'off',
+  },
+};
